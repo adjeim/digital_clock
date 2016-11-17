68238f975586938bbed621348e326b37b9aef677
Mia Adjei <adjei.m@gmail.com>
2016-11-17 16:32:29 -0500

    Added scripts and style for background color.

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
+function placeZero(here) {
+	if (here < 10) {
+		here = "0" + here;
+	}
+
+	return here;
+}
+
+
 function showCurrentTime() {
 
 	var time = new Date();
 
-	var hours = time.getHours();
-	var minutes = time.getMinutes();
-	var seconds = time.getSeconds();
+	var hours = placeZero(time.getHours());
+	var minutes = placeZero(time.getMinutes());
+	var seconds = placeZero(time.getSeconds());
 
 
 	var hexTime = document.getElementsByClassName("showHex")[0];
 	hexTime.innerHTML = "#" + hours + minutes + seconds;
+
+	var bgColor = document.getElementsByTagName("body")[0];
+	bgColor.style.backgroundColor = "#" + hours + minutes + seconds;
+
+	setTimeout(showCurrentTime, 1000);
 
 }
 
 showCurrentTime();
 
 
-
-
-
-
-
-
-
-
-
-
-// hexTime.innerHTML = 
-
-// newDate
